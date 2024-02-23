2db7952f935eb0989f2c95f4dc575a7c07587c85
vaskrsijep <panicvaskrsije@gmail.com>
2024-02-23 13:32:28 +0100

    navigation menu end

--- src/components/Sections/Navbar.tsx
+++ src/components/Sections/Navbar.tsx
@@ -13,7 +13,10 @@ const Navbar = () => {
     {open &&
       <div className="relative w-full bg-white h-full">
         <div className="absolute z-50 w-1/3 right-0 top-16">
-        <motion.ul className="flex items-center justify-center  gap-10 flex-col text-xl h-full bg-blue-100 rounded-lg text-[#1E3240] py-10 hover:text-black shadow-inner" animate={{ opacity: 1, x: 0 }} initial={{ opacity: 0, x: 100 }} transition={{ duration: 0.5 }}> 
+        <motion.ul className="flex items-center justify-center  gap-10 flex-col text-xl h-full bg-blue-100 rounded-lg text-[#1E3240] py-10 hover:text-black shadow-inner" 
+        animate={{ opacity: 1, x: 0 }} 
+        initial={{ opacity: 0, x: 100 }}
+        transition={{ duration: 0.5 }}>  
           <li className="hover:text-[#1E3240]">Home</li>
           <li className="hover:text-[#1E3240]">About</li>
           <li className="hover:text-[#1E3240]">Project</li>
@@ -48,7 +51,7 @@ const Navbar = () => {
           initial={{ opacity: 0, y: -100 }}
           transition={{ duration: 0.5 }}
         >
-          <motion.div className="hamburger md:hidden block cursor-pointer" onClick={handleClick} animate={{ opacity: 1, y: 0 }} initial={{ opacity: 0, y: -100 }} transition={{ duration: 0.5 }}>
+          <motion.div aria-hidden="true" className="hamburger md:hidden block hidd cursor-pointer" onClick={handleClick} animate={{ opacity: 1, y: 0 }} initial={{ opacity: 0, y: -100 }} transition={{ duration: 0.5 }}>
         <motion.span
           className="hamburger-span"
           animate={{ rotate: open ? -45 : 0, translateX: open ? -9 : 0, translateY: open ? 9 : 0 }}
